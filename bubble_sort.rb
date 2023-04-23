c15b91b2afe01957eda57030332acc2920c90da7
Jack Roudebush <roudebushjack@gmail.com>
2023-04-22 22:54:40 -0400

    Add conditional to evaluate sorted variable

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,19 +3,27 @@ require 'pry-byebug'
 sort_me = [8, 5, 9, 3, 1]
 
 def bubble_sort(sort_me)
-    sorted = []
+    sorted_array = []
     n = sort_me.length
     sorted = false
-    swapped = false
-
-    while sorted == false
-        for i in 1..(n - 1) do
-            if sort_me[i - 1] > sort_me[i]
-                sort_me[i - 1], sort_me[i] = sort_me[i], sort_me[i - 1] 
+    
+    until sorted == true do
+        swapped = false
+        for idx in 1..(n - 1) do
+            if sort_me[idx - 1] > sort_me[idx]
+                sort_me[idx - 1], sort_me[idx] = sort_me[idx], sort_me[idx - 1]
+                swapped == true 
+            else sort_me[idx - 1] < sort_me[idx]
+                sort_me[idx - 1], sort_me[idx] = sort_me[idx - 1], sort_me[idx]
             end #if
         end #for
-    end #while
+        for idx in 1..(n - 1) do
+            if sort_me[idx - 1] < sort_me[idx]
+                sorted == true
+            end #if
+        end #for
+    end #until
 end
 
-binding.pry
+# binding.pry
 bubble_sort(sort_me)

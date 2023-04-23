abb90889197e13906710e7a4845d4f92fa86f55b
Jack Roudebush <roudebushjack@gmail.com>
2023-04-22 22:16:46 -0400

    Input main function elements

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,3 +1,12 @@
 def bubble_sort(sort_me)
-    
+    sorted = []
+    n = sort_me.length
+
+    while swapped == false
+        for i in 1..(n - 1) do
+            if sort_me[i - 1] > sort_me[i]
+
+            end #if
+        end #for
+    end #while
 end

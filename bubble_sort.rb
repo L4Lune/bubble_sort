e0ae9e940fc5e0427e563c778bfd76f19cd61165
Jack Roudebush <roudebushjack@gmail.com>
2023-04-22 23:36:27 -0400

    Clean up code

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,6 @@
 require 'pry-byebug'
-# sort_me = Array.new(5) { rand(0..10) }
-sort_me = [8, 5, 9, 3, 1]
+sort_me = Array.new(10) { rand(0..10) }
+# sort_me = [8, 5, 9, 3, 1]
 # sort_me = [3, 1]
 def bubble_sort(sort_me)
     sorted_array = []
@@ -13,13 +13,13 @@ def bubble_sort(sort_me)
             if sort_me[idx - 1] > sort_me[idx]
                 (sort_me[idx - 1], sort_me[idx] = sort_me[idx], sort_me[idx - 1])
                 swapped == true
-                for idx in 1..(n - 1) do
-                    if sort_me[idx - 1] < sort_me[idx]
-                        sorted == true
-                    end #if
-                end #for 
             elsif sort_me[idx - 1] < sort_me[idx]
                 swapped == false
+            end #if
+        end #for
+        for idx in 1..(n - 1) do
+            if sort_me[idx - 1] < sort_me[idx]
+                sorted == true
             end #if
         end #for
         p sort_me

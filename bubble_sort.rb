3af3fc1b20b1ef14cc6e49b83da6adf42cc04156
Jack Roudebush <roudebushjack@gmail.com>
2023-04-22 23:33:48 -0400

    Complete bubble_sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,28 +1,29 @@
 require 'pry-byebug'
-# sort_me = Array.new(5) { rand(0..10) }
-sort_me = [8, 5, 9, 3, 1]
-
+sort_me = Array.new(5) { rand(0..10) }
+# sort_me = [8, 5, 9, 3, 1]
+# sort_me = [3, 1]
 def bubble_sort(sort_me)
     sorted_array = []
     n = sort_me.length
     sorted = false
     
-    until sorted == true do
+    for iteration in 1..(n-1) do    
         swapped = false
         for idx in 1..(n - 1) do
             if sort_me[idx - 1] > sort_me[idx]
-                sort_me[idx - 1], sort_me[idx] = sort_me[idx], sort_me[idx - 1]
-                swapped == true 
-            else sort_me[idx - 1] < sort_me[idx]
-                sort_me[idx - 1], sort_me[idx] = sort_me[idx - 1], sort_me[idx]
+                (sort_me[idx - 1], sort_me[idx] = sort_me[idx], sort_me[idx - 1])
+                swapped == true
+                for idx in 1..(n - 1) do
+                    if sort_me[idx - 1] < sort_me[idx]
+                        sorted == true
+                    end #if
+                end #for 
+            elsif sort_me[idx - 1] < sort_me[idx]
+                swapped == false
             end #if
         end #for
-        for idx in 1..(n - 1) do
-            if sort_me[idx - 1] < sort_me[idx]
-                sorted == true
-            end #if
-        end #for
-    end #until
+        p sort_me
+    end #for
 end
 
 # binding.pry
